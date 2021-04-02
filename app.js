d30076397cf51df6d7183eb89e81a4173ca881e8
john <john@gmail.com>
2021-04-02 07:48:50 +0530

    app.js updated with new code

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const command = process.argv[2]
 //console.log(process.argv)
 if (command === 'add') {
     console.log("in if construct")
+    var y = 20
+    console.log('value of y', y)
     console.log("Adding Post...")
 }
 else if (command === 'remove') {
     console.log('Removing Post...')
+    console.log('this option will remove post from the database')
+}
+
+else if(command === 'update'){
+    console.log('update posts')
 }
